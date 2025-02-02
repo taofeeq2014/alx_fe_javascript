373964f86daddce8c3e2b98aa10096354bee014a
taofeeq2014 <taofeeq2014@outlook.com>
2025-02-02 23:09:37 +0100

    Create script.js

---
+++ dom-manipulation/script.js
@@ -0,0 +1,10 @@
+function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
